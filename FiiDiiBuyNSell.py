5c6a042ed10afd2b8c82540793c8fbf475bb700f
Imraan Khann <imraannkhan0@gmail.com>
2024-10-21 13:52:22 +0530

    Update FiiDiiBuyNSell.py imported pytz

--- FiiDiiBuyNSell.py
+++ FiiDiiBuyNSell.py
@@ -10,6 +10,7 @@ import numpy as np
 import pandas as pd
 #from  telegram import Bot
 import asyncio
+import pytz
 
 # def strRed(skk): return "\033[91m {}\033[00m".format(skk)
 # def strGreen(skk): return "\033[92m {}\033[00m".format(skk)
