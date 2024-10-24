2406cba4424c4df4fc93091b9d4cd4b8dc9d9375
Imraan Khann <imraannkhan0@gmail.com>
2024-10-24 11:03:24 +0530

    Update FiiDiiBuyNSell.py

--- FiiDiiBuyNSell.py
+++ FiiDiiBuyNSell.py
@@ -297,6 +297,6 @@ if intTime >= 9 and intTime < 14:
         
             time.sleep(120)
 
-            if(intTime>=14 or counter == 23):
+            if(intTime>=14 or counter == 73):
                 print("PROGRAM EXIT AT : ", runTm)
                 exit()
